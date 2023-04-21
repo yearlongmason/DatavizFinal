c9c80ce006c2eefef22325474a8fb74c4db3820f
Mason <114454215+yearlongmason@users.noreply.github.com>
2023-04-21 15:05:01 -0400

    Update HarryPotterTextAnalysis.py

--- HarryPotterTextAnalysis.py
+++ HarryPotterTextAnalysis.py
@@ -2,3 +2,4 @@ import streamlit as st
 
 st.set_page_config(page_title="Harry Potter Text Analysis", layout="wide")
 st.title("Harry Potter Script Analysis")
+st.markdown('This is just some text writing a little bit about the project')
