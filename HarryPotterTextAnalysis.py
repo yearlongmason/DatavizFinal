60bf815d26b2a44609219fe03ecce971edff1294
Mason <114454215+yearlongmason@users.noreply.github.com>
2023-04-21 15:12:01 -0400

    Update HarryPotterTextAnalysis.py

--- HarryPotterTextAnalysis.py
+++ HarryPotterTextAnalysis.py
@@ -5,8 +5,6 @@ st.set_page_config(page_title="Harry Potter Text Analysis", layout="wide")
 st.title("Harry Potter Script Analysis")
 st.markdown('This is just some text writing a little bit about the project')
 
-testImage = "https://canvasjs.com/wp-content/uploads/images/gallery/javascript-charts/overview/javascript-charts-graphs-index-data-label.png"
-
-image = Image.open(testImage)
+image = Image.open('hpChart.png')
 
 st.image(image, caption='Fake Chart')
