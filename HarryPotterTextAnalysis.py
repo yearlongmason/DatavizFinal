21cd110f9ccabe0ad6cf16531f681eae980801a5
Mason <114454215+yearlongmason@users.noreply.github.com>
2023-04-21 15:16:47 -0400

    Update HarryPotterTextAnalysis.py

--- HarryPotterTextAnalysis.py
+++ HarryPotterTextAnalysis.py
@@ -5,20 +5,20 @@ st.set_page_config(page_title="Harry Potter Text Analysis", layout="wide")
 st.title("Harry Potter Script Analysis")
 st.markdown('This is just some text writing a little bit about the project')
 
-tab1, tab2, tab3, tab4 = st.tabs(["All 3", "1", "2", "3"])
+tab1, tab2, tab3, tab4 = st.tabs(["All 3 Movies", "Sorcerer's Stone", "Chamber of Secrets", "Prizoner of Azkaban"])
 
 with tab1:
   image = Image.open('hpChart.png')
-  st.image(image, caption='Fake Chart')
+  st.image(image, caption='Temp Chart tab 1')
   
 with tab2:
   image = Image.open('hpChart.png')
-  st.image(image, caption='Fake Chart tab 2')
+  st.image(image, caption='Temp Chart tab 2')
   
 with tab3:
   image = Image.open('hpChart.png')
-  st.image(image, caption='Fake Chart')
+  st.image(image, caption='Temp Chart tab 3')
   
 with tab4:
   image = Image.open('hpChart.png')
-  st.image(image, caption='Fake Chart')
+  st.image(image, caption='Temp Chart tab 4')
