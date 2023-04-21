c18d95a4827121fbea38790cac6bfcb18d2daa71
Mason <114454215+yearlongmason@users.noreply.github.com>
2023-04-21 15:14:56 -0400

    Update HarryPotterTextAnalysis.py

--- HarryPotterTextAnalysis.py
+++ HarryPotterTextAnalysis.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Harry Potter Text Analysis", layout="wide")
 st.title("Harry Potter Script Analysis")
 st.markdown('This is just some text writing a little bit about the project')
 
-tab1, tab2, tab3, tab4 = st.tabs(["All 3", "1", "2", 3])
+tab1, tab2, tab3, tab4 = st.tabs(["All 3", "1", "2", "3"])
 
 with tab1:
   image = Image.open('hpChart.png')
