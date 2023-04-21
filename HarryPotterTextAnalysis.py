cc274fe1359921155e10fd4d639c411b3961bf4e
Mason <114454215+yearlongmason@users.noreply.github.com>
2023-04-21 15:24:59 -0400

    Update HarryPotterTextAnalysis.py
    Adding tab conclusion test

--- HarryPotterTextAnalysis.py
+++ HarryPotterTextAnalysis.py
@@ -16,6 +16,7 @@ with tab1:
   with col2:
     image = Image.open('hpChart.png')
     st.image(image, caption='This will be a description of the chart')
+  st.markdown('This is just some text at the end of each page saying something about the findings of this tab in particular')
   
 with tab2:
   col1, col2 = st.columns(2)
