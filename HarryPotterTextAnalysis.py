258aa83a29c6c62158a964d33eaa7460ac734766
Mason <114454215+yearlongmason@users.noreply.github.com>
2023-04-21 15:14:21 -0400

    Update HarryPotterTextAnalysis.py
    Adding tabs

--- HarryPotterTextAnalysis.py
+++ HarryPotterTextAnalysis.py
@@ -5,6 +5,20 @@ st.set_page_config(page_title="Harry Potter Text Analysis", layout="wide")
 st.title("Harry Potter Script Analysis")
 st.markdown('This is just some text writing a little bit about the project')
 
-image = Image.open('hpChart.png')
+tab1, tab2, tab3, tab4 = st.tabs(["All 3", "1", "2", 3])
 
-st.image(image, caption='Fake Chart')
+with tab1:
+  image = Image.open('hpChart.png')
+  st.image(image, caption='Fake Chart')
+  
+with tab2:
+  image = Image.open('hpChart.png')
+  st.image(image, caption='Fake Chart tab 2')
+  
+with tab3:
+  image = Image.open('hpChart.png')
+  st.image(image, caption='Fake Chart')
+  
+with tab4:
+  image = Image.open('hpChart.png')
+  st.image(image, caption='Fake Chart')
