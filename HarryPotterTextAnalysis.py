9bd0beaf48dd21c888506b794cfa9bf9b00760a9
Mason <114454215+yearlongmason@users.noreply.github.com>
2023-04-25 13:24:29 -0400

    Update HarryPotterTextAnalysis.py
    Add title to altair test chart

--- HarryPotterTextAnalysis.py
+++ HarryPotterTextAnalysis.py
@@ -59,6 +59,7 @@ with tab1:
     source = pd.DataFrame({'a': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],'b': [28, 55, 43, 91, 81, 53, 19, 87, 52]})
     chart = alt.Chart(source).mark_bar().encode(x='a', y='b')
     chart = chart.properties(width=700, height=375) #Set figure size
+    chart = chart.properties(title='HP Data')
     st.altair_chart(chart)
     
   st.markdown('This is just some text at the end of each page saying something about the findings of this tab in particular')
