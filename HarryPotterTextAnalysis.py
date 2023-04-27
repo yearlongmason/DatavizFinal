8f9a7ff3ae32204092e145fce84963f1dd18d496
Mason <114454215+yearlongmason@users.noreply.github.com>
2023-04-26 22:43:23 -0400

    Update HarryPotterTextAnalysis.py
    Linewidth on violin plot

--- HarryPotterTextAnalysis.py
+++ HarryPotterTextAnalysis.py
@@ -334,7 +334,7 @@ def numWordsVP(data):
     if 'Hufflepuff' in list(data['House']):
         houses = ['Gryffindor', 'Slytherin', 'Ravenclaw', 'Hufflepuff', 'Muggle']
         colors = ['#be0119', '#009500', '#069af3', '#feb308', '#5f6b73']
-    sns.violinplot(data=data, x='House', y='numWords', linewidth=2.5, palette=colors, order = houses)
+    sns.violinplot(data=data, x='House', y='numWords', linewidth=1.5, palette=colors, order = houses)
     #ax.set_title('Number of Words Spoken Per Line by House', fontsize=18, fontweight='bold', loc='left', color='xkcd:grey')
     ax.set_ylabel('Number of Words per Line', fontsize=16, fontweight='bold')
     ax.set_xlabel('House', fontsize=16, fontweight='bold')
