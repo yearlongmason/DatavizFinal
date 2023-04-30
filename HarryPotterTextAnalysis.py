5e924bfbbe1efa28762ef36f82d72db2a94df33e
Mason <114454215+yearlongmason@users.noreply.github.com>
2023-04-30 13:35:49 -0400

    Update HarryPotterTextAnalysis.py
    Adding a section explaining the central question

--- HarryPotterTextAnalysis.py
+++ HarryPotterTextAnalysis.py
@@ -491,8 +491,8 @@ st.markdown('##### Why this project?')
 st.markdown("As a kid, I was always a big fan of the Harry Potter movies. It was something about the idea of magic, the worldbuilding, and the aesthetic that came with the movies that was really enjoyable as a kid, and it was something I never really stopped enjoying. With that in mind, I figured it could be fun to do some sort of analysis of the movies. What started as a fun project to work on in my free time ended up turning into my final project for my data visualization class!")
 st.markdown('##### The Data')
 st.markdown("What is this data about, anyway? Essentially, the data to start off with contained every line from the first three Harry Potter movies as well as the character that said it. To start off with, this was not a whole lot of data; however, it offered a lot of potential for creating new data, such as \"house\", which is just what house the character is in, and \"numWords\", which is the number of words in the sentence, which is ideally supposed to give insight into the amount of content in a specific line.")
-st.markdown('##### The Data')
-st.markdown('')
+st.markdown('##### What is this looking at?')
+st.markdown('What am I trying to do with all of this information? It\'s no secret that the Harry Potter movies are mostly dominated by Gryffindor. Gryffindor is cool and all, but wouldn\'t it be cool to hear from some of the other houses too? I\'m hoping to explore and learn more about how different houses are represented across the first three Harry Potter movies. More specifically, I would like to show how the representation of houses in the Harry Potter movies evolves and changes as we progress further into the wizarding world!')
 
 
 tab1, tab2, tab3, tab4 = st.tabs(["Sorcerer's Stone", "Chamber of Secrets", "Prizoner of Azkaban", "First 3 Movies Combined"])
